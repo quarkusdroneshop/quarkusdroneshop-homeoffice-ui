a733f9945850b8080aae7da5701e49e3be6d2c4d
Noriaki Mushino <nmushino@redhat.com>
2025-06-11 13:07:49 +0900

    Update common.js

--- webpack.common.js
+++ webpack.common.js
@@ -132,7 +132,7 @@ module.exports = env => {
           { from: './src/app/favicon.ico', to: 'images' },
         ]
       }),
-      new webpack.EnvironmentPlugin({REACT_APP_GRAPHQL_ENDPOINT: 'http://localhost:4000/graphql'})
+      new webpack.EnvironmentPlugin({REACT_APP_GRAPHQL_ENDPOINT: 'http://localhost:8080/graphql'})
     ],
     resolve: {
       extensions: ['.js', '.ts', '.tsx', '.jsx'],
